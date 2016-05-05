f8aa133632650b16132aaa95b3673cd7f88c9a20
Pat Cavit <pcavit@gmail.com>
2016-05-05 00:13:29 -0700

    Zip packages, nicer output

--- build/package.js
+++ build/package.js
@@ -1,17 +1,23 @@
+/* eslint no-console:0 */
 "use strict";
 
 var packager = require("electron-packager"),
+    humanize = require("humanize-duration"),
+    zipdir   = require("zip-dir"),
+    size     = require("filesize"),
     
-    pkg = require("../package.json");
+    pkg = require("../package.json"),
+    
+    start = Date.now();
     
 packager({
+    platform  : "win32,linux",
     arch      : "all",
     asar      : true,
     dir       : "./app",
     icon      : "./logo/hat",
     out       : "./packages",
     overwrite : true,
-    platform  : "win32,linux",
     prune     : true,
     
     download : {
@@ -33,5 +39,32 @@ packager({
         return process.exit(1);
     }
     
-    console.log(`Wrote:\n${paths.join("\n")}`);
+    console.log(`\nBuilt ${paths.length} package(s) in ${humanize(Date.now() - start)}:\n\t${paths.join("\n\t")}\n`);
+    
+    start = Date.now();
+    
+    console.log("Zipping packages...");
+    
+    return Promise.all(paths.map((dir) => {
+        var out = `${dir}.zip`;
+        
+        return new Promise((resolve, reject) => {
+            zipdir(dir, {
+                saveTo : out
+            }, (err, buffer) => {
+                if(err) {
+                    return reject(err);
+                }
+                
+                console.log(`Zipped ${out} (${size(buffer.length)}) in ${humanize(Date.now() - start)}`);
+                
+                return resolve(out);
+            });
+        });
+    }))
+    .catch((err) => {
+        console.error(err);
+        
+        return process.exit(1);
+    });
 });
